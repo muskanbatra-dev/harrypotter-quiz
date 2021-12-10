107623b1652749363fc89b08ef0a5b0320cd0e74
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-10 08:35:33 +0530

    update

---
+++ app.js
@@ -0,0 +1,239 @@
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May i have your name?');
+console.log(userName );
+var welcomemessage= "Welcome to HARRY POTTER QUIZ " + userName;//string concatenation
+console.log(welcomemessage);
+
+
+var readlineSync = require('readline-sync')
+var score = 0;
+
+var questionOne = (" How many Hogwarts houses are there?");
+var answerone = "four";
+
+
+// input
+var userAnswer = readlineSync.question(questionOne);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerone) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+
+
+var questiontwo = ("Who is Harry Potter's godfather??");
+var answertwo = "Sirius Black";
+
+
+// input
+var userAnswer = readlineSync.question(questiontwo);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answertwo) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionthree = ("Who created the Sorting Hat??");
+var answerthree = "Godric Gryffindor";
+
+
+// input
+var userAnswer = readlineSync.question(questionthree);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerthree) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionfour = ("What is the name of the train that takes students to Hogwarts?");
+var answerfour = "The Hogwarts Express"
+
+
+// input
+var userAnswer = readlineSync.question(questionfour);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerfour) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionfive = ("What is Snape’s Patronus?");
+var answerfive = "A doe";
+
+
+// input
+var userAnswer = readlineSync.question(questionfive);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerfive) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionsix = ("Which type of insect is Ronald Weasley most afraid of?");
+var answersix = "Spiders";
+
+
+// input
+var userAnswer = readlineSync.question(questionsix);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answersix) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionseven = ("What is the name of Albus Dumbledore’s brother?");
+var answerseven = "Aberforth Dumbledore";
+
+
+// input
+var userAnswer = readlineSync.question(questionseven);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerseven) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questioneight = ("What position did Harry Potter play at Quidditch?");
+var answereight = "Seeker";
+
+
+// input
+var userAnswer = readlineSync.question(questioneight);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answereight) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionnine = ("How many horcruxes does Voldemort make?");
+var answernine = "seven Horcruxes";
+
+
+// input
+var userAnswer = readlineSync.question(questionnine);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answernine) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+
+var questionten = ("?");
+var answerten = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionten);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerten) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+
+
+
+
+
+
